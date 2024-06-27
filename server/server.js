d09423a32b0f9bc15990ac555daa66adba879bc0
alexfinger21 <cdx9933@gmai1.com>
2024-06-27 19:51:13 -0400

    hello world on express

--- server/server.js
+++ server/server.js
@@ -1,1 +1,11 @@
-console.log("Hello World");
+const express = require("express")
+
+const app = express()
+
+app.get("/", (req, res) => {
+    return res.send("Hello World")
+})
+
+app.listen(3000, () => {
+  console.log(`695 server listening on port ${3000}`)
+})
